341558e0d376babc6c7eba459bd0a45ed5706bf3
zulvlad <zulfiia.vladimirova2gmail.com>
2020-12-15 22:29:05 +0600

    Таблица умножения на 1

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args){
+        System.out.println("Zulfiia Vladimirova");
+        System.out.println("15.12.2020");
+    }
+}
